c25eac2a3489cdf6178df9300ae576a9075a1fd1
David L Gibbs <Gibbsdavidl@gmail.com>
2021-07-16 14:41:06 -0700

    Update S1_stats.sql

--- Analysis/SQL/S1_stats.sql
+++ Analysis/SQL/S1_stats.sql
@@ -1,3 +1,5 @@
+# saved query: S1 stat table
+# note: uses the v2 tables.
 SELECT
   a.Study,
   a.EdgeID,
@@ -8,20 +10,16 @@ SELECT
   b.median AS late_med,
   b.mad AS late_mad,
   a.median - b.median AS med_diff,
-  (a.median - b.median) / SQRT(POW(a.mad,2) + POW(b.mad,2)) AS S1
+  (a.median - b.median) / SQRT( 1.4826*a.mad  + 1.4826*b.mad ) AS S1
 FROM
-  `isb-cgc-02-0001.Cytokine_Network_Analysis.net_wts_summary_early_stage_by_study`  a
+  `isb-cgc-02-0001.Cytokine_Network_Analysis_v2.summary_by_study` a
 JOIN
-  `isb-cgc-02-0001.Cytokine_Network_Analysis.net_wts_summary_late_stage_by_study`  b
+  `isb-cgc-02-0001.Cytokine_Network_Analysis_v2.summary_by_NOT_study` b
 ON
-  a.EdgeID = b.EdgeID
-  AND a.Study = b.Study
+  a.EdgeID = b.EdgeID and a.Study = b.Study
 WHERE
   (a.mad IS NOT NULL
     AND b.mad IS NOT NULL)
-  AND a.mad > 0
-  AND b.mad > 0
+  AND (a.mad > 0 OR b.mad > 0)
 GROUP BY
 1,2,3,4,5,6,7,8
-ORDER BY
-S1 DESC
